59fc55e6e360bfbd18574e31af5f3916237d1c0c
Aspurgus <75339765+Aspurgus@users.noreply.github.com>
2022-01-09 20:23:47 -0500

    Movement

---
+++ src/main/java/com/aspr/dynamictreesassorted/TreesAssorted.java
@@ -0,0 +1,22 @@
+package com.aspr.dynamictreesassorted;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TreesAssorted.MODID)
+public class TreesAssorted
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final String MODID = "dynamictreesassorted";
+
+    public TreesAssorted() {
+
+
+    }
+}
